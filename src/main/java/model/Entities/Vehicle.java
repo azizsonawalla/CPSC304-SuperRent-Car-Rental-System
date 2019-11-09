db6b9fdc67be7de536ec464cf362dfdefb31c77d
George <george.sam.chen@gmail.com>
2019-11-09 14:02:58 -0800

    Project Skeleton

--- src/main/java/model/Entities/Vehicle.java
+++ src/main/java/model/Entities/Vehicle.java
@@ -1,9 +1,5 @@
 package model.Entities;
 
-<<<<<<< HEAD:src/main/java/model/Entities/Car.java
-public class Car {
-    int dlicense;
-=======
 public class Vehicle {
->>>>>>> origin/master:src/main/java/model/Entities/Vehicle.java
+    int dlicense;
 }

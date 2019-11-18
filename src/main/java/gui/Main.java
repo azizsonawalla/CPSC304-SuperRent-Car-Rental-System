9da077c91a88e9485ef786c6b3ccbe86a2550d06
Aziz Sonawalla <sonawallaa@gmail.com>
2019-11-18 02:33:26 -0800

    clerk reservation and rental search UI

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("views/customerMakeReservation.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/clerkReservationRentalSearch.fxml"));
         primaryStage.setTitle(GUIConfig.APP_TITLE);
         Scene scene = new Scene(root, GUIConfig.WINDOW_WIDTH, GUIConfig.WINDOW_HEIGHT);
         scene.getStylesheets().add(getClass().getResource("views/style.css").toExternalForm());

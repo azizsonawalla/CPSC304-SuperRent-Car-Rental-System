5adade1a20520384f66089fbca895ae48ba0f3ad
Aziz Sonawalla <sonawallaa@gmail.com>
2019-11-18 16:59:09 -0800

    initial scene switching logic

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -1,4 +1,20 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    private Main main;
+    @FXML private Button switchToClerkButton;
+
+    public Controller(Main main) {
+        this.main = main;
+    }
+    
+    @FXML
+    private void switchToClerk(ActionEvent event) throws Exception {
+        main.setRoot("views/clerkReservationRentalSearch.fxml");
+    }
 }
--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -2,21 +2,35 @@ package gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Scene scene;
+    Stage primaryStage;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("views/clerkDailyReports.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/customerCarSearch.fxml"));
+        loader.setController(new Controller(this));
+        Pane root = loader.load();
         primaryStage.setTitle(GUIConfig.APP_TITLE);
-        Scene scene = new Scene(root, GUIConfig.WINDOW_WIDTH, GUIConfig.WINDOW_HEIGHT);
+        this.scene = new Scene(root, GUIConfig.WINDOW_WIDTH, GUIConfig.WINDOW_HEIGHT);
         scene.getStylesheets().add(getClass().getResource("views/style.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+        this.primaryStage = primaryStage;
+    }
+
+    public void setRoot(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+        loader.setController(new Controller(this));
+        Pane root = loader.load();
+        scene.setRoot(root);
+        //primaryStage.show();
     }
 
 

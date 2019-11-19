a2ab6ba6b10112fcdb6c0815f55832fa0a38c43f
Aziz Sonawalla <sonawallaa@gmail.com>
2019-11-18 16:35:21 -0800

    daily reports

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("views/clerkMakeReservation.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/clerkDailyReports.fxml"));
         primaryStage.setTitle(GUIConfig.APP_TITLE);
         Scene scene = new Scene(root, GUIConfig.WINDOW_WIDTH, GUIConfig.WINDOW_HEIGHT);
         scene.getStylesheets().add(getClass().getResource("views/style.css").toExternalForm());

e2ad4a69e87b5b112706d9e525249f731e5e04cf
George <george.sam.chen@gmail.com>
2019-11-24 14:03:44 -0800

    added comments for queries + added GET_RESERVATION/RENTAL/RETURNS_WITH queries

--- src/main/java/model/Queries.java
+++ src/main/java/model/Queries.java
@@ -112,28 +112,34 @@ public class Queries {
 
     public static class Reservation {
 
-        //query to add reservations
+        //Add a new reservation tuple to the Reservations table. confNo autoincrements.
+        /*
+        Used in:
+            1) Make a reservation
+         */
         public static String ADD_RESERVATION =
                 "INSERT INTO Reservations(vtName, dLicense, fromDateTime, toDateTime, city, location) " +
                         "VALUES (?, ?, ?, ?, ?, ?)";
 
-        //query to update reservations
+        //Update existing tuple in reservations table given a confNo.
         public static String UPDATE_RESERVATION =
                 "UPDATE Reservations " +
                         "SET vtname = ?, dLicense = ?, fromDateTime = ?, toDatetime = ?, city = ?, location = ? " +
                         "WHERE confNo = ?";
 
-        //query to delete reservations
+        //Delete existing tuple from reservations table given a confNo.
         public static String DELETE_RESERVATION =
                 "DELETE FROM Reservations " +
                         "WHERE confNo = ?";
 
-        //get reservations matching
+        //Query reservation tuple from Reservation with a given confNo
         public static String GET_RESERVATION =
                 "SELECT * " +
                         "FROM Reservation " +
                         "WHERE confNo = ?";
 
+        //Query active reservations (any reservation that has not been started as long as the end time of the reservation
+        //is after the current time)
         public static String GET_ACTIVE_RESERVATIONS =
                 "SELECT * " +
                         "FROM Reservations " +
@@ -141,72 +147,99 @@ public class Queries {
                         "AND confNo NOT IN (SELECT r.confNo FROM Rent r)";
 
         //Get reservations that overlap with the given time period at a given location for a given vehicle type
+        /*
+        Used in:
+            1) View the number of available vehicles for a specific car type, location, and time interval
+            2) Generate "Daily Rentals" report
+            3) Generate "Daily Rentals for Branch" report
+            4) Generate "Daily Returns" report
+            5) Generate "Daily Returns for Branch" report
+         */
         public static String GET_RESERVATIONS_WITH =
                 "SELECT * " +
                         "FROM Reservations " +
                         "WHERE ((R.fromDateTime <= ? AND R.toDateTime > ?) OR (R.toDateTime >= ? AND R.fromDateTime < ?)) " +
                         "AND R.vtName = ? AND R.location = ? AND R.city = ?";
 
-//        public static String GET_RECENT_RESERVATIONS =
-//                        "SELECT * " +
-//                        "FROM Reservations " +
-//                        "WHERE confNo = (SELECT MAX(confNo) " +
-//                                        "FROM Reservations)";
-
     }
 
     public static class Rent {
 
-
+        //Add a new rent tuple to the Rent table. rId autoincrements.
+        /*
+        Used In:
+            1) Renting a Vehicle
+         */
         public static String ADD_RENTAL =
                 "INSERT INTO Rent(vLicense, dLicense, fromDateTime, toDateTime, odometer, cardNo, confNo)" +
                         "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
+        //Update existing tuple in Rent table given a rId.
         public static String UPDATE_RENTAL =
                 "UPDATE Rent" +
                         "SET vLicense = ?, dLicense = ?, fromDateTime = ?, toDateTime = ?, odometer = ?" +
                         ", cardNo = ?, confNo = ?" +
                         "WHERE rId = ?";
 
+        //Delete existing tuple in Rent table given a rId.
         public static String DELETE_RENTAL =
                 "DELETE FROM Rent " +
                         "WHERE rId = ?";
 
+
+        //Query Rent tuple with a given rId
         public static String GET_RENTAL =
                 "SELECT * " +
                         "FROM Rent " +
                         "WHERE rId = ?";
 
+        //Query active rentals (any rent that has not been completed and returned as long as the end time is after the
+        //current time)
         public static String GET_ACTIVE_RENTALS =
                 "SELECT * " +
                         "FROM Rent " +
                         "WHERE rId = ? AND dLicense = ? AND ? <= toDateTime " +
                         "AND rId NOT IN (SELECT rId FROM Returns)";
 
+        //Query rentals that were made in a given time period
+        /*
+        Used In:
+            1) Generate "Daily Rentals" report
+            2) Generate "Daily Rentals for Branch" report
+         */
         public static String GET_RENTALS_TODAY =
                 "SELECT * " +
                         "FROM Rent R " +
                         "WHERE ? <= R.fromDateTime AND ? >= R.fromDateTime";
 
-        public static String GET_RECENT_RENTALS =
+        //Get rentals that overlap with the given time period at a given location for a given vehicle type
+        /*
+        Used in:
+            1) Generate "Daily Returns" report
+            2) Generate "Daily Returns for Branch" report
+         */
+        public static String GET_RENTALS_WITH =
                 "SELECT * " +
-                        "FROM Rent " +
-                        "WHERE rId = (SELECT MAX(rId) " +
-                                      "FROM Rent)";
+                        "FROM Rent R, Reservations V " +
+                        "WHERE R.confNo = V.confNo AND ((R.fromDateTime <= ? AND R.toDateTime > ?) OR (R.toDateTime >= ? AND R.toDateTime < ?)) " +
+                        "AND V.vtName = ? AND V.location = ? AND V.city = ?";
 
         }
 
     public static class Customer {
 
+        //Add a new customer to the Customer table.
         public static String ADD_CUSTOMER =
                 "INSERT INTO Customer(cellphone, name, address, dLicense) " +
                         "VALUES (?,?,?,?)";
 
+        //Update an existing customer tuple in the Customer table.
         public static String UPDATE_CUSTOMER =
                 "UPDATE Customer " +
                         "SET cellphone = ?, name = ?, address = ? " +
                         "WHERE dLicense = ?";
 
+        //Delete an existing customer tuple from the Customer table.
         public static String DELETE_CUSTOMER = "DELETE FROM Customer " +
                 "WHERE dLicense = ?";
 
@@ -214,29 +247,46 @@ public class Queries {
 
     public static class Vehicle {
 
+        //Add a new vehicle to the Vehicle table.
         public static String ADD_VEHICLE = "INSERT INTO Vehicle(vId, vLicense, make, model, year, color, odometer, " +
                 "vtName, status, location, city) " +
                 "VALUES (?,?,?,?,?,?,?,?,?,?,?)";
 
+        //Update an existing vehicle tuple in the Vehicle table.
+        /*
+        Used in:
+            1) Returning a Vehicle
+         */
         public static String UPDATE_VEHICLE = "UPDATE Vehicle " +
                 "SET vId = ?, make = ?, model = ?, year = ?, color = ?, odometer = ?, status = ? " +
                 "WHERE vLicense = ?";
 
+        //Delete an existing vehicle tuple from the Vehicle table.
         public static String DELETE_VEHICLE = "DELETE FROM Vehicle " +
                 "WHERE vLicense = (?)";
 
+        //Get vehicles with a given type at a given location and of a given status
         public static String GET_VEHICLES_WITH =
                 "SELECT * " +
                         "FROM Vehicle " +
                         "WHERE vtName = ? AND location = ? AND city = ? AND status = ?";
 
         //Get the total number of vehicles at a given location that are a given vehicle type
+        /*
+        Used in:
+            1) View the number of available vehicles for a specific car type, location, and time interval
+         */
         public static String GET_NUM_VEHICLES_WITH =
                 "SELECT V.vtName, V.location, V.city, COUNT(*) " +
                         "FROM Vehicle V " +
                         "WHERE vtName = ? AND location = ? AND city = ? " +
                         "GROUP BY V.vtName, V.location, V.city";
 
+        //Query vehicle tuple with a given vLicense
+        /*
+        Used in:
+            1) Returning a Vehicle
+         */
         public static String GET_VEHICLE =
                 "SELECT * " +
                         "FROM Vehicle " +
@@ -247,20 +297,24 @@ public class Queries {
 
     public static class VehicleType {
 
+        //Add a new vehicle type to the VehicleType table.
         public static String ADD_VEHICLE_TYPE =
                 "INSERT INTO VehicleType(vtName, features, wRate, dRate, hRate, wInsRate, dInsRate, hInsRate, kRate)" +
                         "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
+        //Update an existing vehicle type tuple in the VehicleType table.
         public static String UPDATE_VEHICLE_TYPE =
                 "UPDATE VehicleType " +
                         "SET features = ?, wRate = ?, dRate = ?, " +
                         "hRate = ?, wInsRate = ?, dInsRate = ?, hInsRate = ?, kRate = ? " +
                         "WHERE vtName = ?";
 
+        //Delete an existing vehicle type tuple from the VehicleType table.
         public static String DELETE_VEHICLE_TYPE =
                 "DELETE FROM VehicleType " +
                         "WHERE vtName = ?";
 
+        //Query all vehicle type tuples
         public static String QUERY_ALL =
                 "SELECT * FROM VehicleType";
 
@@ -268,45 +322,75 @@ public class Queries {
 
     public static class Returns {
 
+        //Add a new return to the Returns table.
+        /*
+        Used in:
+            1) Returning a Vehicle
+         */
         public static String ADD_RETURN =
                 "INSERT INTO Returns(rId, dateTime, odometer, fullTank, value) " +
                         "VALUES (?, ?, ?, ?, ?)";
 
+        //Update an existing return tuple in the Returns table.
         public static String UPDATE_RETURN =
                 "UPDATE Returns" +
                         "SET dateTime = ?, odometer = ?, fullTank = ?, value = ?" +
                         "WHERE rId = ?";
 
+        //Delete an existing return tuple from the Returns table.
         public static String DELETE_RETURN =
                 "DELETE FROM Returns " +
                         "WHERE rId = ?";
 
+        //Query all return tuples with a given rId
         public static String GET_RETURN =
                 "SELECT * " +
                         "FROM Returns " +
                         "WHERE rId = ?";
 
+        //Query all tuples joining Returns with Rent by rId for a given rId
+        /*
+        Used in:
+            1) Returning a Vehicle
+         */
         public static String JOIN_RENTAL =
                 "SELECT * " +
                         "FROM Returns RT, Rent R " +
                         "WHERE RT.rId = R.rId AND RT.rId = ?";
+
+        //Query all tuples natural joining returns, rent, and reservations within a given time period, of a given vehicle
+        //type, and at a given branch
+        /*
+        Used in:
+            1) Generate "Daily Returns" report
+            2) Generate "Daily Returns for Branch" report
+         */
+        public static String GET_RETURNS_WITH =
+                "SELECT * " +
+                        "FROM Returns R, Rent N, Reservations V " +
+                        "WHERE R.rId = N.rId AND N.confNo = V.confNo AND (R.dateTime >= ?) AND (R.dateTime <= ?) AND " +
+                        "AND V.vtName = ? AND AND V.location = ? AND V.city = ?";
     }
 
     public static class Card {
 
+        //Add a new card to the Card table.
         public static String ADD_CARD =
                 "INSERT INTO Card(cardNo, cardName, expDate) " +
                         "VALUES (?, ?, ?)";
 
+        //Update an existing card tuple in the Card table.
         public static String UPDATE_CARD =
                 "UPDATE Card" +
                         "SET cardName = ?, expDate = ? " +
                         "WHERE cardNo = ?";
 
+        //Delete an existing card tuple from the Card table.
         public static String DELETE_CARD =
                 "DELETE FROM Card " +
                         "WHERE cardNo = ?";
 
+        //Query a card with a given cardNo from the Card table
         public static String GET_CARD =
                 "SELECT * " +
                         "FROM Card " +
@@ -315,42 +399,30 @@ public class Queries {
 
     public static class Branch {
 
+        //Add a new branch to the Branch table.
         public static String ADD_BRANCH =
                 "INSERT INTO Branch(city, location) " +
                         "VALUES(?, ?)";
 
+        //Update an existing branch tuple in the Branch table.
         public static String UPDATE_BRANCH =
                 "UPDATE Branch" +
                         "SET city = , location = ? " +
                         "WHERE city = ? AND location = ?";
 
+        //Delete an existing branch tuple from the Branch table.
         public static String DELETE_BRANCH =
                 "DELETE FROM Branch " +
                         "WHERE city = ? AND location = ?";
 
+        //Query a branch from the Branch table with a given city and location
         public static String GET_BRANCH =
                         "SELECT * FROM Branch " +
                         "WHERE city = ? AND location = ?";
 
+        //Query all branch tuples from the Branch table
         public static String GET_ALL_BRANCHES =
                 "SELECT * " +
                         "FROM Branch";
-    }
-
-    public static class CustomerTransactions {
-        //TODO: Find the number of vehicles for any combination of: car type, location, and time interval
-
-        //TODO: Decide on/generate confirmation number
-
-    }
-
-    public static class ClerkTransactions {
-        //TODO: Generate daily rentals report
-
-        //TODO: Generate daily rentals report for branch
-
-        //TODO: Generate daily returns report
-
-        //TODO: Generate daily returns report for branch
     }
 }
